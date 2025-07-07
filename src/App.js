3e8babb589d18ac5d9abef7857bfad757184e528
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-07 20:28:42 +0600

    react.js simple practice

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: 'md soriful islam',
+    job: 'web developer'
+  }
+  var person2 = {
+    name: 'Eva Rahman',
+    job: 'singer'
+  }
+  var beautifulKorbo = {
+    color: 'red',
+    backgroundColor: 'cyan',
+    padding: 20,
+    margin: 20
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +23,10 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Website</h1>
+        <h1 style={{backgroundColor: 'red', color: 'cyan'}}>My heading: {person.name + ' ' + person.job}</h1>
+        <h3 style={beautifulKorbo}>{person2.name + ' ' + person2.job}</h3>
+        <p>my first react paragraph</p>
       </header>
     </div>
   );
