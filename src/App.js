b2fa0bdf04e29a54fe044efdedb266c33123b05a
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-07 22:41:00 +0600

    my component practice

--- src/App.js
+++ src/App.js
@@ -2,34 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var person = {
-    name: 'md soriful islam',
-    job: 'web developer'
-  }
-  var person2 = {
-    name: 'Eva Rahman',
-    job: 'singer'
-  }
-  var beautifulKorbo = {
-    color: 'red',
-    backgroundColor: 'cyan',
-    padding: 20,
-    margin: 20
-  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>My Website</h1>
-        <h1 style={{backgroundColor: 'red', color: 'cyan'}}>My heading: {person.name + ' ' + person.job}</h1>
-        <h3 style={beautifulKorbo}>{person2.name + ' ' + person2.job}</h3>
-        <p>my first react paragraph</p>
+    <div className='App'>
+      <header className='App-header'>
+        <p>I am a React Person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
-
+function Person(){
+  return(
+  <div style= {{border: '2px solid red', backgroundColor: 'cyan', color: 'black', padding: '30px', margin: '10px'}}>
+    <h1>Name: Sakib khan, All hasan</h1>
+    <h3>Hero of the year</h3>
+  </div>
+  )
+}
 export default App;
